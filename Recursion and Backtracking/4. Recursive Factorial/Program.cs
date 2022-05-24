ec503c92817c33522669f8321ce4a7cc274e18a7
Nenko Pakov <nenko.pakov@gmail.com>
2022-05-24 11:17:47 +0300

    Factorial task

---
+++ Recursion and Backtracking/4. Recursive Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4._Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalculateFactorial(number)); 
+        }
+
+        private static int CalculateFactorial(int number)
+        {
+            if (number<=1)
+            {
+                return 1;
+            }
+
+            return number * CalculateFactorial(number - 1);
+        }
+    }
+}
