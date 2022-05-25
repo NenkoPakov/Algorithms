afe521ad77f5a5fbb6aec35e19e995afb98f9de1
Nenko Pakov <nenko.pakov@gmail.com>
2022-05-25 15:25:47 +0300

    Fibonacci number

---
+++ Recursion and Backtracking/7. Recursive Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _7._Recursive_Fibonacci
+{
+    class Program
+    {
+        private static int FibonacciNumber;
+
+        static void Main()
+        {
+            FibonacciNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFibonacci(FibonacciNumber));
+        }
+
+        private static int GetFibonacci(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return 1;
+
+            }
+
+            return GetFibonacci(number - 1) + GetFibonacci(number - 2);
+        }
+    }
+}
