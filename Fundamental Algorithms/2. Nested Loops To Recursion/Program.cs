d13869c3b507d94b2252ae1c6d79c012cbd75560
Nenko Pakov <nenko.pakov@gmail.com>
2022-05-30 14:01:50 +0300

    Finished NestedLoops

---
+++ Fundamental Algorithms/2. Nested Loops To Recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._Nested_Loops_To_Recursion
+{
+    public static class Program
+    {
+        private static int NestedLoopsCount;
+        private static int[] result;
+        static void Main()
+        {
+            NestedLoopsCount = int.Parse(Console.ReadLine());
+            result = new int[NestedLoopsCount];
+            GenerateNestedLoopPermotation(0);
+        }
+
+        private static void GenerateNestedLoopPermotation(int currentIndex)
+        {
+            if (currentIndex >= NestedLoopsCount)
+            {
+                Console.WriteLine(string.Join(' ', result));
+                return;
+            }
+
+            for (int i = 1; i <= NestedLoopsCount; i++)
+            {
+                result[currentIndex] = i;
+                GenerateNestedLoopPermotation(currentIndex + 1);
+            }
+        }
+    }
+}
