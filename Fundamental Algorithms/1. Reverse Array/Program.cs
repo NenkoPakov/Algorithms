1e4d6f15532aa1da50690a67a51ef51f058154f5
Nenko Pakov <nenko.pakov@gmail.com>
2022-05-30 13:12:47 +0300

    Reverse array

--- Fundamental Algorithms/1. Reverse Array/Program.cs
+++ Fundamental Algorithms/1. Reverse Array/Program.cs
@@ -6,7 +6,25 @@ namespace _1._Reverse_Array
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Using string[] we can reverse even sentences, not just numbers
+            string[] input = Console.ReadLine().Split();
+            int startingIndex = 0;
+            ReverseArray(input, startingIndex);
+            Console.WriteLine(string.Join(' ', input));
+        }
+
+        private static void ReverseArray(string[] input, int startIndex)
+        {
+            int inputLength = input.Length;
+
+            if (startIndex != inputLength / 2)
+            {
+                string temporaryElement = input[inputLength - 1 - startIndex];
+                input[inputLength - 1 - startIndex] = input[startIndex];
+                input[startIndex] = temporaryElement;
+
+                ReverseArray(input, startIndex + 1);
+            }
         }
     }
 }
