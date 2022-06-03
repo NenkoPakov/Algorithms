165cb4bb66e18d4c333128a48353e099bfb05c42
Nenko Pakov <nenko.pakov@gmail.com>
2022-06-03 16:11:39 +0300

    Sum of coins and Set cover

---
+++ Fundamental Algorithms/7. Sum of Coins/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _7._Sum_of_Coins
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            IEnumerable<int> coins = Console.ReadLine().Split(", ").Select(x => int.Parse(x));
+
+            int targetValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetCoinsSum(coins.OrderByDescending(x => x), targetValue));
+        }
+
+        private static string GetCoinsSum(IEnumerable<int> orderedCoins, int targetValue)
+        {
+            int coinIndex = default;
+
+            IDictionary<int, int> takenCoins = new Dictionary<int, int>();
+
+            while (targetValue > 0 && coinIndex < orderedCoins.Count())
+            {
+                int currentCoin = orderedCoins.Skip(coinIndex).FirstOrDefault();
+
+                int coinCount = targetValue / currentCoin;
+
+                if (coinCount == 0)
+                {
+                    coinIndex++;
+                    continue;
+                }
+
+                targetValue %= currentCoin;
+
+                takenCoins.Add(currentCoin, coinCount);
+                coinIndex++;
+            }
+
+            if (targetValue > 0)
+            {
+                return "Error";
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Number of coins to take: {takenCoins.Sum(x => x.Value)}");
+
+                foreach (var key in takenCoins.Keys)
+                {
+                    sb.AppendLine($"{takenCoins[key]} coin(s) with value {key}");
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ Fundamental Algorithms/8. Set Cover/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8._Set_Cover
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            List<int> universe = Console.ReadLine()
+                                        .Split(", ")
+                                        .Select(x => int.Parse(x))
+                                        .ToList();
+
+            int subSetCount = int.Parse(Console.ReadLine());
+            int[][] subSets = new int[subSetCount][];
+
+            ICollection<int[]> usedSets = new List<int[]>();
+
+            for (int set = 0; set < subSetCount; set++)
+            {
+                subSets[set] = Console.ReadLine()
+                                    .Split(", ")
+                                    .Select(x => int.Parse(x))
+                                    .ToArray();
+            }
+
+            GetCoverSets(universe, subSets, usedSets);
+
+            PrintTheResult(universe, usedSets);
+        }
+
+        private static void GetCoverSets(List<int> universe, int[][] subSets, ICollection<int[]> usedSets)
+        {
+            int[] currentSet = subSets
+                                                .OrderByDescending(set => set.Count(elem => universe.Contains(elem)))
+                                                .FirstOrDefault(set => set.Any(elem => universe.Contains(elem)));
+
+            while (universe.Any() && currentSet != null)
+            {
+                usedSets.Add(currentSet);
+
+                foreach (var elem in from int elem in currentSet
+                                     where universe.Contains(elem)
+                                     select elem)
+                {
+                    universe.Remove(elem);
+                }
+
+                currentSet = subSets
+                                         .OrderByDescending(set => set.Count(elem => universe.Contains(elem)))
+                                         .FirstOrDefault(set => set.Any(elem => universe.Contains(elem)));
+            }
+        }
+
+        private static void PrintTheResult(List<int> universe, ICollection<int[]> usedSets)
+        {
+            if (universe.Any())
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                Console.WriteLine($"Sets to take ({usedSets.Count}):");
+
+                foreach (int[] set in usedSets)
+                {
+                    Console.WriteLine(string.Join(", ", set));
+                }
+            }
+        }
+    }
+}
