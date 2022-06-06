8174d63819187f6b8f9809987a1bace3fe7db73e
Nenko Pakov <nenko.pakov@gmail.com>
2022-06-06 15:51:30 +0300

    3. Cycles in a Graph

--- Fundamental Algorithms/3. Cycles in a Graph/Program.cs
+++ Fundamental Algorithms/3. Cycles in a Graph/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _3._Cycles_in_a_Graph
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private static Dictionary<char,ICollection<char>> graph = new Dictionary<char, ICollection<char>>();
+        private static HashSet<char> visited = new HashSet<char>();
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string input;
+
+            while ((input = Console.ReadLine())!= "End")
+            {
+                char[] edge = input.Split('-').Select(x=>char.Parse(x)).ToArray();
+
+                char fromVertex = edge[0];
+                char toVertex = edge[1];
+
+                if (!graph.ContainsKey(fromVertex))
+                {
+                    graph.Add(fromVertex, new List<char>());
+                }
+
+                if (!graph.ContainsKey(toVertex))
+                {
+                    graph.Add(toVertex, new List<char>());
+                }
+
+                graph[fromVertex].Add(toVertex);
+            }
+
+            try
+            {
+                DFS(graph.Keys.FirstOrDefault());
+                Console.WriteLine($"Acyclic: Yes");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"Acyclic: No"); 
+            }
+        }
+
+        private static void DFS(char node)
+        {
+            ICollection<char> children = graph[node];
+
+            if (visited.Contains(node))
+            {
+                throw new InvalidOperationException("Acyclic graph");
+            }
+
+            visited.Add(node);
+
+            foreach (char child in children)
+            {
+                DFS(child);
+            }
         }
     }
 }
